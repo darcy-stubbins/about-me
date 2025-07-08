a6c9d8414f54b944438cc2598b50895703f0a784
darcy-stubbins <darcystubbins.dev.99@gmail.com>
2025-07-08 14:06:49 +0100

    added the bulk of the info in and restructured pages slightly for layout purposes

--- resources/views/skills.blade.php
+++ resources/views/skills.blade.php
@@ -18,7 +18,24 @@
     @include('partials.navbar')
     @include('partials.sidebar')
 
-    <div class="header">Professional Summary and Skills</div>
+    <div class="header">Skills</div>
+    <div class="card">
+        <ul>
+            <li>PHP/ Laravel</li>
+            <br>
+            <li>CSS/ Bootstrap/ Tailwind</li>
+            <br>
+            <li>JavaScript/ jQuery</li>
+            <br>
+            <li>NPM/ Node</li>
+            <br>
+            <li>Linux/ Debian</li>
+            <br>
+            <li>Docker</li>
+            <br>
+            <li>MySQL/ SQLite</li>
+        </ul>
+    </div>
 </body>
 
 </html>
